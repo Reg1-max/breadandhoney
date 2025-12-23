d0cf75440abdeda0fcbf8bef22cf78a1bb22fd16
Regina <qregi.1@outlook.com>
2025-12-23 23:34:55 +0000

    added tables to store budget in database

--- breadandhoney/budget/models.py
+++ breadandhoney/budget/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Income(models.Model):
+    incomeID = models.BigAutoField(primary_key=True)
+    salary = models.DecimalField(decimal_places=2, max_digits=11)
+    bonus = models.DecimalField(decimal_places=2, max_digits=11)
+    other = models.DecimalField(decimal_places=2, max_digits=11)
+
+class Outgoings(models.Model):
+    outID = models.BigAutoField(primary_key=True)
+    accom = models.DecimalField("Accommodation", max_digits=11, decimal_places=2)
+    utilities = models.DecimalField(decimal_places=2, max_digits=11)
+    travel = models.DecimalField(decimal_places=2, max_digits=11)
+    groceries = models.DecimalField(decimal_places=2, max_digits=11)
+    entertainment = models.DecimalField(decimal_places=2, max_digits=11)
+    other = models.DecimalField(decimal_places=2, max_digits=11)
