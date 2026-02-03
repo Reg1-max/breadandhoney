c7da10207efc4cc216946a79aa8f2a6675dbbeae
Regina <qregi.1@outlook.com>
2026-02-03 19:40:58 +0000

    change CharField to DecimalField for proper validation

--- breadandhoney/budget/forms.py
+++ breadandhoney/budget/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 
 class Quiz1Form(forms.Form):
-    salary = forms.CharField(label="salary")
-    bonus = forms.CharField(label="bonus")
-    other = forms.CharField(label="other")
+    salary = forms.DecimalField(label="salary",max_digits=11,decimal_places=2)
+    bonus = forms.DecimalField(label="bonus",max_digits=11,decimal_places=2)
+    other = forms.DecimalField(label="other",max_digits=11,decimal_places=2)
 
 class Quiz2Form(forms.Form):
-    accom = forms.CharField(label="Accommodation")
-    utilities = forms.CharField(label="Utilities")
-    travel = forms.CharField(label="Travel")
-    groceries = forms.CharField(label="Groceries")
-    entertainment = forms.CharField(label="Entertainment")
-    other = forms.CharField(label="Other")
+    accom = forms.DecimalField(label="Accommodation",max_digits=11,decimal_places=2)
+    utilities = forms.DecimalField(label="Utilities",max_digits=11,decimal_places=2)
+    travel = forms.DecimalField(label="Travel",max_digits=11,decimal_places=2)
+    groceries = forms.DecimalField(label="Groceries",max_digits=11,decimal_places=2)
+    entertainment = forms.DecimalField(label="Entertainment",max_digits=11,decimal_places=2)
+    other = forms.DecimalField(label="Other",max_digits=11,decimal_places=2)
